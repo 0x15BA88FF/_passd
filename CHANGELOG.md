12c8f57ad22ec48fadfdfeb8c3abbbfabfa1136c
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-01-06 11:48:45 +0000

    Adde: add passd `list_items` command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,6 +20,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Add passd `list_items` command.
 - Add passd `find_items` command.
 - Add passd `generate_password` command.
 - Add passd `move_item` command.
---
+++ src/commands/list_items.rs
@@ -0,0 +1,59 @@
+use std::{
+    fs,
+    io,
+    path::{Path, PathBuf}
+};
+
+#[derive(Debug)]
+pub struct EntryInfo {
+    pub name: String,
+    pub path: PathBuf,
+    pub r#type: EntryType,
+    pub children: Option<Vec<EntryInfo>>
+}
+
+#[derive(PartialEq, Debug)]
+pub enum EntryType {
+    File,
+    Directory
+}
+
+pub fn list_items(
+    path: &Path,
+    recursive: Option<bool>,
+) -> Result<Vec<EntryInfo>, io::Error> {
+    let mut items = Vec::new();
+    let recursive = recursive.unwrap_or(false);
+
+    if !path.is_dir() {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Provided path is not a directory",
+        ));
+    }
+
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let metadata = entry.metadata()?;
+        let entry_type = if metadata.is_dir() {
+            EntryType::Directory
+        } else {
+            EntryType::File
+        };
+
+        let children = if recursive && entry_type == EntryType::Directory {
+            Some(list_items(&entry.path(), Some(true))?)
+        } else {
+            None
+        };
+
+        items.push(EntryInfo {
+            name: entry.file_name().to_string_lossy().to_string(),
+            path: entry.path(),
+            r#type: entry_type,
+            children,
+        });
+    }
+
+    Ok(items)
+}
