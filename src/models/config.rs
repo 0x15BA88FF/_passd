7eb8a01c80a980d8908b61598bfe8d7832ed0bfe
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-16 19:16:32 +0000

    feat: add configuration modal

---
+++ src/models/config.rs
@@ -0,0 +1,15 @@
+use serde::Deserialize;
+use crate::model::metadata::BaseMetadata;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub dir: String,
+    pub logs_dir: String,
+    pub metadata_dir: String,
+
+    pub port: u16,
+    pub enable_tls: bool,
+
+    #[serde(default)]
+    pub metadata_template: Option<BaseMetadata>,
+}
