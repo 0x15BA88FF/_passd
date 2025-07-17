f7862b1bd37a8f9c584b8b6b448c62fa1731ce8d
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-17 09:00:48 +0000

    feat: implement defaults for config model

--- src/models/config.rs
+++ src/models/config.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use std::path::PathBuf;
 use crate::model::metadata::BaseMetadata;
 
 #[derive(Debug, Deserialize)]
@@ -15,4 +16,31 @@ pub struct Config {
 
     #[serde(default)]
     pub metadata_template: Option<BaseMetadata>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        let base = dirs::home_dir()
+            .map(|home| home.join(".passd"))
+            .unwrap_or_else(|| PathBuf::from(".passd"));
+
+        let vault_dir = base.join("vault");
+        let logs_dir = base.join("logs");
+        let keys_dir = base.join(".keys");
+        let metadata_dir = vault_dir.join(".metadata");
+
+        Self {
+            vault_dir: vault_dir,
+            logs_dir: logs_dir,
+            metadata_dir: metadata_dir,
+
+            public_key_path: keys_dir.join("public.pem"),
+            private_key_path: keys_dir.join("private.pem"),
+
+            port: 7117,
+            enable_tls: true,
+
+            metadata_template: Some(BaseMetadata::default()),
+        }
+    }
 }
