ab89151940afcd1cc0a006e927511e2a0452822f
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-17 21:39:56 +0000

    feat: add simple configuration resolution and loader functions

---
+++ src/configs/mod.rs
@@ -0,0 +1,28 @@
+use config::{Config as RawConfig, File};
+use crate::models::config::Config;
+use directories::BaseDirs;
+use dirs;
+use std::{env, path::PathBuf};
+
+pub fn resolve_config_path() -> Option<PathBuf> {
+    [
+        env::var("PASSD_CONFIG_DIR").ok()
+            .map(|dir: String| PathBuf::from(dir).join("config.toml")),
+        BaseDirs::new()
+            .map(|base: BaseDirs| base.config_dir().join("passd/config.toml")),
+        dirs::home_dir()
+            .map(|home: PathBuf| home.join(".passd/config.toml")),
+    ].into_iter().flatten().find(|path| path.exists())
+}
+
+pub fn load_config_simple() -> Result<Config, Box<dyn std::error::Error>> {
+    let Some(config_path) = resolve_config_path() else {
+        return Ok(Config::default());
+    };
+    let raw = RawConfig::builder()
+        .add_source(File::from(config_path))
+        .build()?;
+    let config = raw.try_deserialize()?;
+
+    Ok(config)
+}
