405cd8333435128fb01fb3550584df578d177aa1
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-19 12:18:54 +0000

    fmt: format config methods

--- src/configs/mod.rs
+++ src/configs/mod.rs
@@ -1,18 +1,21 @@
-use config::{Config as RawConfig, File};
 use crate::models::config::Config;
+use config::{Config as RawConfig, File};
 use directories::BaseDirs;
 use dirs;
 use std::{env, path::PathBuf};
 
 pub fn resolve_config_path() -> Option<PathBuf> {
     [
-        env::var("PASSD_CONFIG_DIR").ok()
+        env::var("PASSD_CONFIG_DIR")
+            .ok()
             .map(|dir: String| PathBuf::from(dir).join("config.toml")),
         BaseDirs::new()
             .map(|base: BaseDirs| base.config_dir().join("passd/config.toml")),
-        dirs::home_dir()
-            .map(|home: PathBuf| home.join(".passd/config.toml")),
-    ].into_iter().flatten().find(|path| path.exists())
+        dirs::home_dir().map(|home: PathBuf| home.join(".passd/config.toml")),
+    ]
+    .into_iter()
+    .flatten()
+    .find(|path| path.exists())
 }
 
 pub fn load_config() -> Result<Config, Box<dyn std::error::Error>> {
