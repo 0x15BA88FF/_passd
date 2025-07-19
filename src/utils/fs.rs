2eda770acc4bc6ae2c5b164f83154ec6ce83f335
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-19 16:13:57 +0000

    feat: add utilities for secure file and directory creation

---
+++ src/utils/fs.rs
@@ -0,0 +1,37 @@
+use std::{
+    fs::{self, Permissions},
+    os::unix::fs::PermissionsExt,
+    path::{Path, PathBuf},
+}
+
+pub fn secure_create_dir_all(path: &Path) -> std::io::Result<()> {
+    fs::create_dir_all(path)?;
+
+    let mut current = PathBuf::new();
+
+    for part in path.components() {
+        current.push(part);
+        if current.is_dir() {
+            fs::set_permissions(&current, Permissions::from_mode(0o700))?;
+        }
+    }
+
+    Ok(())
+}
+
+pub fn secure_write_file(
+    path: &Path,
+    content: impl AsRef<[u8]>
+) -> std::io::Result<()> {
+    use std::fs::OpenOptions;
+    use std::io::Write;
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .mode(0o600)
+        .open(path)?;
+
+    file.write_all(content.as_ref())
+}
