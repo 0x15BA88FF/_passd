7a3ec3d19593f30f8e3d7d6b0bcb5bd71da52b18
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-19 12:30:58 +0000

    feat: add secret constructor

--- src/models/secret.rs
+++ src/models/secret.rs
@@ -1,4 +1,14 @@
+use std::path::{Path, PathBuf};
+
 #[derive(Debug, Clone)]
 pub struct Secret {
     pub relative_path: PathBuf,
+}
+
+impl Secret {
+    pub fn new(
+        relative_path: impl Into<PathBuf>
+    ) -> Self {
+        Self { relative_path: relative_path.into() }
+    }
 }
