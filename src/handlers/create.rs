9f39a7806b654cc8e59f51aab90f5a3b18b03884
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-20 15:31:06 +0000

    feat: add json rpc create secret method

--- src/handlers/create.rs
+++ src/handlers/create.rs
@@ -1,7 +1,53 @@
-use jsonrpsee::server::Extensions;
-use jsonrpsee::types::Params;
-use log::info;
+use jsonrpsee::Extensions;
+use jsonrpsee::types::{ErrorObject, Params};
+use log::{error, info};
+use passd::models::{metadata::BaseMetadata, secret::Secret};
+use serde::Deserialize;
 
-pub fn handler(_params: Params, _ctx: &(), _ext: &Extensions) -> &'static str {
-    "Hello, World!"
+#[derive(Debug, Deserialize)]
+struct CreateParams {
+    path: String,
+    content: String,
+    metadata: BaseMetadata,
+    public_key: Option<String>,
+}
+
+pub fn handler(
+    params: Params,
+    _ctx: &(),
+    _ext: &Extensions,
+) -> Result<String, ErrorObject<'static>> {
+    let create_params: CreateParams = params.parse().map_err(|e| {
+        error!("Failed to parse create parameters: {}", e);
+
+        ErrorObject::owned(
+            jsonrpsee::types::error::INVALID_PARAMS_CODE,
+            "Invalid parameters",
+            Some(format!("Failed to parse parameters: {}", e)),
+        )
+    })?;
+
+    match (Secret {
+        relative_path: create_params.path.into(),
+    })
+    .create(
+        &create_params.content,
+        &create_params.metadata,
+        create_params.public_key.as_deref(),
+    ) {
+        Ok(_) => {
+            info!("Create operation successful");
+
+            Ok("Item created successfully".to_string())
+        }
+        Err(e) => {
+            error!("Create operation failed: {}", e);
+
+            Err(ErrorObject::owned(
+                jsonrpsee::types::error::INTERNAL_ERROR_CODE,
+                "Create operation failed",
+                Some(e.to_string()),
+            ))
+        }
+    }
 }
