e3958a46e64b42580291393346da9e3bb42166ca
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-21 13:14:59 +0000

    feat: add json rpc clone secret handler

---
+++ src/handlers/clone_to.rs
@@ -0,0 +1,68 @@
+use jsonrpsee::Extensions;
+use jsonrpsee::types::{ErrorObject, Params};
+use log::{error, info};
+use passd::models::secret::Secret;
+use serde::Deserialize;
+use std::path::PathBuf;
+
+#[derive(Debug, Deserialize)]
+struct CloneParams {
+    from_path: String,
+    to_path: String,
+    public_key: String,
+    private_key: Option<String>,
+    password: String,
+}
+
+pub fn handler(
+    params: Params,
+    _ctx: &(),
+    _ext: &Extensions,
+) -> Result<String, ErrorObject<'static>> {
+    let clone_params: CloneParams = params.parse().map_err(|e| {
+        error!("Failed to parse parameters: {}", e);
+
+        ErrorObject::owned(
+            jsonrpsee::types::error::INVALID_PARAMS_CODE,
+            "Invalid parameters",
+            Some(format!("Failed to parse parameters: {}", e)),
+        )
+    })?;
+
+    match (Secret {
+        relative_path: clone_params.from_path.clone().into(),
+    })
+    .clone_to(
+        PathBuf::from(&clone_params.to_path),
+        &clone_params.public_key,
+        clone_params.private_key.as_deref(),
+        &clone_params.password,
+    ) {
+        Ok(_) => {
+            info!(
+                "Successfully cloned secret from {} to {}",
+                clone_params.from_path, clone_params.to_path
+            );
+
+            Ok(format!(
+                "Successfully cloned secret from {} to {}",
+                clone_params.from_path, clone_params.to_path
+            ))
+        }
+        Err(e) => {
+            error!(
+                "Failed to clone secret from {} to {}: {}",
+                clone_params.from_path, clone_params.to_path, e
+            );
+
+            Err(ErrorObject::owned(
+                jsonrpsee::types::error::INTERNAL_ERROR_CODE,
+                format!(
+                    "Failed to clone secret from {} to {}",
+                    clone_params.from_path, clone_params.to_path
+                ),
+                Some(e.to_string()),
+            ))
+        }
+    }
+}
--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,10 +1,11 @@
 use anyhow::Result;
 use jsonrpsee::RpcModule;
 
+pub mod clone_to;
+pub mod copy_to;
 pub mod create;
 pub mod delete;
 pub mod move_to;
-pub mod copy_to;
 pub mod read;
 pub mod update;
 
@@ -15,6 +16,7 @@ pub fn register_handlers(module: &mut RpcModule<()>) -> Result<()> {
     module.register_method("read", read::handler)?;
     module.register_method("move", move_to::handler)?;
     module.register_method("copy", copy_to::handler)?;
+    module.register_method("clone", clone_to::handler)?;
 
     Ok(())
 }
