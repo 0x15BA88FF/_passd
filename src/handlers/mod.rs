f4688ae0141e78f26acb33effdd04e797c954c45
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-21 11:42:48 +0000

    feat: add json rpc read secret method

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -3,12 +3,14 @@ use jsonrpsee::RpcModule;
 
 pub mod create;
 pub mod delete;
+pub mod read;
 pub mod update;
 
 pub fn register_handlers(module: &mut RpcModule<()>) -> Result<()> {
     module.register_method("create", create::handler)?;
     module.register_method("update", update::handler)?;
     module.register_method("delete", delete::handler)?;
+    module.register_method("read", read::handler)?;
 
     Ok(())
 }
---
+++ src/handlers/read.rs
@@ -0,0 +1,77 @@
+use jsonrpsee::Extensions;
+use jsonrpsee::types::{ErrorObject, Params};
+use log::{error, info};
+use passd::models::{metadata::Metadata, secret::Secret};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize)]
+pub struct ReadResponse {
+    content: String,
+    metadata: Metadata,
+}
+
+#[derive(Debug, Deserialize)]
+struct ReadParams {
+    path: String,
+    private_key: String,
+    password: String,
+}
+
+pub fn handler(
+    params: Params,
+    _ctx: &(),
+    _ext: &Extensions,
+) -> Result<ReadResponse, ErrorObject<'static>> {
+    let read_params: ReadParams = params.parse().map_err(|e| {
+        error!("Failed to parse parameters: {}", e);
+
+        ErrorObject::owned(
+            jsonrpsee::types::error::INVALID_PARAMS_CODE,
+            "Invalid parameters",
+            Some(format!("Failed to parse parameters: {}", e)),
+        )
+    })?;
+
+    let secret = Secret {
+        relative_path: read_params.path.clone().into(),
+    };
+
+    let metadata = match secret.metadata() {
+        Ok(metadata) => {
+            info!("Successfully read secret metadata {}", read_params.path);
+
+            metadata
+        }
+        Err(e) => {
+            error!("Failed to read secret metadata {}: {}", read_params.path, e);
+
+            return Err(ErrorObject::owned(
+                jsonrpsee::types::error::INTERNAL_ERROR_CODE,
+                format!("Failed to read secret metadata {}", read_params.path),
+                Some(e.to_string()),
+            ));
+        }
+    };
+
+    let content = match secret.content(
+        Some(&read_params.private_key),
+        &read_params.password
+    ) {
+        Ok(content) => {
+            info!("Successfully read secret content {}", read_params.path);
+
+            content
+        }
+        Err(e) => {
+            error!("Failed to read secret content {}: {}", read_params.path, e);
+
+            return Err(ErrorObject::owned(
+                jsonrpsee::types::error::INTERNAL_ERROR_CODE,
+                format!("Failed to read secret content {}", read_params.path),
+                Some(e.to_string()),
+            ));
+        }
+    };
+
+    Ok(ReadResponse { content, metadata })
+}
