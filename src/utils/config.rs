2361db289f84e4094de01732ecbed495ec2ba9e8
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-24 10:52:39 +0000

    refactor: move resolve config path method to utils

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,6 +1,5 @@
 use directories::BaseDirs;
 use dirs;
-use log::{info, warn};
 use std::{env, path::PathBuf};
 
 pub fn resolve_config_paths() -> Option<PathBuf> {
@@ -12,19 +11,5 @@ pub fn resolve_config_paths() -> Option<PathBuf> {
         dirs::home_dir().map(|home| home.join(".passd/config.toml")),
     ];
 
-    let found_path = config_paths
-        .into_iter()
-        .flatten()
-        .find(|path| path.exists());
-
-    match found_path {
-        Some(ref path) => {
-            info!("Successfully resolved configuration {}", path.display());
-            found_path
-        }
-        None => {
-            warn!("Failed to resolve configuration file");
-            None
-        }
-    }
+    config_paths.into_iter().flatten().find(|path| path.exists());
 }
