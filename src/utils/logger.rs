e6ff4ad2222f6840089fcf0dc2581458a813af84
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-25 18:25:50 +0000

    refactor: use anyhow for logging return

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,12 +1,10 @@
+use anyhow::{Context, Result};
 use chrono::Local;
 use fern::Dispatch;
 use log::LevelFilter;
 use std::{fs::OpenOptions, path::PathBuf};
 
-pub fn init_logger(
-    log_file: &PathBuf,
-    log_level: &String,
-) -> Result<(), Box<dyn std::error::Error>> {
+pub fn init_logger(log_file: &PathBuf, log_level: &String) -> Result<()> {
     let log_level = match log_level.to_lowercase().as_str() {
         "debug" => LevelFilter::Debug,
         "error" => LevelFilter::Error,
@@ -15,6 +13,7 @@ pub fn init_logger(
         "info" => LevelFilter::Info,
         _ => LevelFilter::Info,
     };
+
     let mut base_config = Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
@@ -26,14 +25,19 @@ pub fn init_logger(
         })
         .level(log_level)
         .chain(std::io::stdout());
+
     let log_file = OpenOptions::new()
         .write(true)
         .create(true)
         .append(true)
-        .open(log_file)?;
+        .open(log_file)
+        .context("Failed to open log file")?;
 
     base_config = base_config.chain(log_file);
-    base_config.apply()?;
+
+    base_config
+        .apply()
+        .context("Failed to apply logger configuration")?;
 
     Ok(())
 }
