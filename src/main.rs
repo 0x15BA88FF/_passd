7da4faa5dba7dacef18f96cb0a74cfff9e8ddffd
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-25 19:47:21 +0000

    refactor: clean main and use anyhow context

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use jsonrpsee::{RpcModule, server::ServerBuilder};
 use log::info;
-use passd::{configs::load_config, utils::logger::init_logger};
+use passd::{models::config::Config, utils::logger::init_logger};
 use std::net::SocketAddr;
 
 mod handlers;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = load_config().expect("Failed to load config");
-    let _ = init_logger(&config.log_file, &config.log_level)
-        .expect("Failed to initialize logger");
-    let addr: SocketAddr = format!("127.0.0.1:{}", &config.port).parse()?;
+    let config =
+        Config::load_config().context("Failed to load configuration")?;
+
+    init_logger(&config.log_file, &config.log_level)
+        .context("Failed to initialize logger")?;
+
+    let addr: SocketAddr = format!("{}:{}", config.address, config.port)
+        .parse()
+        .context("Failed to parse server address")?;
+
     let mut module = RpcModule::new(());
 
-    handlers::register_handlers(&mut module)?;
+    handlers::register_handlers(&mut module)
+        .context("Failed to register handlers")?;
 
-    let server = ServerBuilder::default().build(addr).await?;
+    let server = ServerBuilder::default()
+        .build(addr)
+        .await
+        .context("Failed to build server")?;
+
     let handle = server.start(module);
 
     info!("Server running on {}", addr);
 
     handle.stopped().await;
-
     Ok(())
 }
