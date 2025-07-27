32f23354163fd966cb88e4a37bdc029a9d3468ad
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-07-27 17:23:02 +0000

    refactor: create macro for handler registration

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -7,18 +7,30 @@ pub mod clone_to;
 pub mod copy_to;
 pub mod create;
 pub mod delete;
+pub mod find;
 pub mod move_to;
 pub mod read;
 pub mod update;
 
+macro_rules! register {
+    ($module:ident, {
+        $($name:literal => $handler:path),* $(,)?
+    }) => {
+        $($module.register_method($name, $handler)?;)*
+    };
+}
+
 pub fn register_handlers(module: &mut RpcModule<Arc<Config>>) -> Result<()> {
-    module.register_method("create", create::handler)?;
-    module.register_method("update", update::handler)?;
-    module.register_method("delete", delete::handler)?;
-    module.register_method("read", read::handler)?;
-    module.register_method("move", move_to::handler)?;
-    module.register_method("copy", copy_to::handler)?;
-    module.register_method("clone", clone_to::handler)?;
+    register!(module, {
+        "create" => create::handler,
+        "update" => update::handler,
+        "delete" => delete::handler,
+        "read"   => read::handler,
+        "move"   => move_to::handler,
+        "copy"   => copy_to::handler,
+        "clone"  => clone_to::handler,
+        "find"   => find::handler,
+    });
 
     Ok(())
 }
