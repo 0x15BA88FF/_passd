a2d27427a34310bf17e543556f19e20ba4ccf3bd
0x15ba88 <22256354-0x15ba@users.noreply.gitlab.com>
2025-08-21 07:52:31 +0000

    feat: add diagnostics JSON RPC handler

---
+++ src/handlers/diagnose.rs
@@ -0,0 +1,33 @@
+use crate::Config;
+use jsonrpsee::{
+    Extensions,
+    types::{ErrorObject, Params},
+};
+use passd::models::secrets::Secrets;
+use serde_json::Value;
+use std::sync::Arc;
+
+pub fn handler(
+    _params: Params,
+    ctx: &Arc<Config>,
+    _ext: &Extensions,
+) -> Result<Value, ErrorObject<'static>> {
+    match (Secrets {
+        config: Arc::clone(ctx),
+    }
+    .diagnose())
+    {
+        Ok(diagnostics) => serde_json::to_value(diagnostics).map_err(|e| {
+            ErrorObject::owned(
+                jsonrpsee::types::error::INTERNAL_ERROR_CODE,
+                "Failed to serialize diagnostics",
+                Some(e.to_string()),
+            )
+        }),
+        Err(e) => Err(ErrorObject::owned(
+            jsonrpsee::types::error::INTERNAL_ERROR_CODE,
+            format!("Unexpected diagnostics error {e}"),
+            Some(e.to_string()),
+        )),
+    }
+}
