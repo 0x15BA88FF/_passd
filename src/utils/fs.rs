37609c5f6914b29db83f4a5296e9cacf49f12b06
0x15ba88 <22256354-0x15ba@users.noreply.gitlab.com>
2025-08-21 07:51:56 +0000

    feat: util functions to validate is secure path

--- src/utils/fs.rs
+++ src/utils/fs.rs
@@ -9,7 +9,6 @@ pub fn secure_create_dir_all(
     base_path: &Path,
 ) -> std::io::Result<()> {
     fs::create_dir_all(path)?;
-
     let relative_path = match path.strip_prefix(base_path) {
         Ok(rel) => rel,
         Err(_) => {
@@ -19,17 +18,13 @@ pub fn secure_create_dir_all(
             ));
         }
     };
-
     let mut current = base_path.to_path_buf();
-
     for component in relative_path.components() {
         current.push(component);
-
         if current.exists() && current.is_dir() {
             fs::set_permissions(&current, Permissions::from_mode(0o700))?;
         }
     }
-
     Ok(())
 }
 
@@ -39,13 +34,51 @@ pub fn secure_write(
 ) -> std::io::Result<()> {
     use std::fs::OpenOptions;
     use std::io::Write;
-
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
         .truncate(true)
         .mode(0o600)
         .open(path)?;
-
     file.write_all(content.as_ref())
+}
+
+pub fn is_secure_dir(path: &Path) -> bool {
+    if let Ok(metadata) = fs::metadata(path) {
+        if metadata.is_dir() {
+            let permissions = metadata.permissions();
+            let mode = permissions.mode() & 0o777;
+
+            return mode != 0o700;
+        }
+    }
+
+    true
+}
+
+pub fn is_secure_file(path: &Path) -> bool {
+    if let Ok(metadata) = fs::metadata(path) {
+        if metadata.is_file() {
+            let permissions = metadata.permissions();
+            let mode = permissions.mode() & 0o777;
+
+            return mode != 0o600;
+        }
+    }
+
+    true
+}
+
+pub fn set_secure_dir_permissions<P: AsRef<Path>>(
+    path: P,
+) -> std::io::Result<()> {
+    let perms = fs::Permissions::from_mode(0o700);
+    fs::set_permissions(path, perms)
+}
+
+pub fn set_secure_file_permissions<P: AsRef<Path>>(
+    path: P,
+) -> std::io::Result<()> {
+    let perms = fs::Permissions::from_mode(0o600);
+    fs::set_permissions(path, perms)
 }
